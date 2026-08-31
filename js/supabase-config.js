bbf75c453d11a07e9f7ead197cee5b67b7391cef
Assessments-R <ramya.quantstrainer@gmail.com>
2026-08-31 12:28:20 +0530

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://zvudfxxnucdbqakppvdg.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_xHuN2sIkTBwCaHnNG3xXcQ_rFqbH7ff";
 
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
